e008b3cc6c6ea53b3f80700316457859a5aa878c
vvdam <maaalouladam@gmail.com>
2024-05-30 09:49:56 +0200

    les ratz

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter");
+const fighters = [
+    new Fighter("🤺Heracles", 20, 6),
+    new Fighter("🦁Nemean Lion", 11, 13),
+];
+
+let attacker = fighters[0];
+let defender = fighters[1];
+
+while (attacker.pv > 0 && defender.pv > 0) {
+    attacker.attack(defender);
+    [attacker, defender] = [defender, attacker];
+}
+
+console.log(
+    `${attacker.pv > 0 ? attacker.name : defender.name} a gagné la bagarre !`
+);
+
+attacker.isAlive();
+
+defender.isAlive();
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,38 @@
 /* Fighter class definition */
+// The fighter will hit with more or less success each time, the number of points of damage that the ** attacker ** does will therefore be a random number between 1 and the strength of the fighter (use Math.random () (Documentation, and if needed some advices from this article)
+
+// But the attacked can defend and dodge! You will therefore mitigate the damage by subtracting the damage from the dexterity of the attacked (without never going below zero)
+
+// Once the damage is calculated, decrease the number of life points of the attacked by the value thus obtained. Be careful, the life of a fighter cannot fall below zero, also remember to check that
+class Fighter {
+    constructor(name, strength, dexterity) {
+        this.name = name;
+        this.pv = 100;
+        this.strength = strength;
+        this.dexterity = dexterity;
+    }
+
+    attack(nemesis) {
+        let damage =
+            Math.floor(Math.random() * this.strength) + 1 - nemesis.dexterity;
+        if (damage > 0) {
+            nemesis.pv -= damage;
+            console.log(
+                `${this.name} éteint ${nemesis.name} avec un coup de ${damage} points de degat`
+            );
+        } else {
+            console.log(`${this.name} rate`);
+        }
+    }
+    isAlive() {
+        if (this.pv <= 0) {
+            console.log(`${this.name} c'est fait allumer ${this.pv}`);
+        } else {
+            console.log(
+                `${this.name} est malheuresement toujour en vie  ${this.pv} `
+            );
+        }
+    }
+}
+
+module.exports = Fighter;
